1b35ce09c27764fb5e98763756cc0d8ce0ecb248
jomariepolanco <jomariepolanco@gmail.com>
2020-10-12 13:49:50 -0400

    done

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,12 @@
 require_relative "./vehicle.rb"
+#requiring Vehicle class here BECAUSE car class needs access to Vehicle class
+#car class will need access to the file that contains that class
+
+class Car < Vehicle # use < to inherit the Car class from Vehicle
+#no methods defined in the Car class
+
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
+#considered a subclass of Vehicle class
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,19 @@
 class Vehicle
 
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, wheel_number) #instances of Vehicle initialize with a wheel size and wheel number
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+
+    def go #common vehicle behavior
+        "vrrrrrrrooom!"
+    end
+
+    def fill_up_tank #common vehicle behavior
+        "filling up!"
+    end
+
 end
+#super class 
